67d01e2cf422aed67cc559050d303f88de6874a0
lene <lene@pois.org.ru>
2011-10-23 15:16:33 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainingPlanDairy::Application.config.secret_token = 'd759437559d9f64f2b0c3d4bb9c6be2b16029cdde6cbe95454470fda70fbda5e3e60630ddd6a02ac1d3a1ad871581d00583b1165995be9acee6c1ef7ae11c918'
